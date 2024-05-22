76aecbd3aa33aab0977c031da009b8fcaa2779a8
AlejandroSergioLeal <alejandro.sergioleal@hotmail.com>
2024-05-21 23:06:15 -0300

    error comillas dobles en vercel

--- app/ui/Footer.tsx
+++ app/ui/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='border-t border-gray-200  bg-zinc-50 dark:bg-zinc-900'>
          <div className='flex flex-col items-center '>
             <p className='text-center p-1 text-bold text-xs'> 
-            Copyright © 2024 "Vinyl Paradise". All rights reserved
+            Copyright © 2024 'Vinyl Paradise'. All rights reserved
             </p>
         </div>
     </footer>
