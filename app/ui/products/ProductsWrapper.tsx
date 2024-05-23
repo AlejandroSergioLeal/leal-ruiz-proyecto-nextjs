cd62b425d7ae7430d396b8203f96f4c7c00e39ad
AlejandroSergioLeal <alejandro.sergioleal@hotmail.com>
2024-05-23 18:43:31 -0300

    primer fetch2

--- app/ui/products/ProductsWrapper.tsx
+++ app/ui/products/ProductsWrapper.tsx
@@ -11,7 +11,7 @@ export default function ProductsWrapper({ products }: ProductWrapperProps) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
       {products.map((product) => (
-        <ProductCard key={product.product_name} product={product} />
+        <ProductCard key={product.name} product={product} />
       ))}
     </div>
   );
--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,6 +1,5 @@
 
 export type Product = {
-  id: number,
   name: string,
   artist: string,
   image: string,
